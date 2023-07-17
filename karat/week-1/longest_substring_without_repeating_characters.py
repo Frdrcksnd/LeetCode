bf110faf839c97c13b657458d619db4ce0bb6465
Frederick Osunde <frederick.osunde@workday.com>
2023-07-17 10:19:29 -0400

    solution for longest substring

---
+++ karat/week-1/longest_substring_without_repeating_characters.py
@@ -0,0 +1,33 @@
+#https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+def lengthOfLongestSubstring(self, s: str) -> int:
+    lookup = set()
+    left = right = 0
+    length = 0
+
+    while right < len(s):
+        while s[right] in lookup:
+            lookup.remove(s[left])
+            left += 1
+        length = max(length, right - left + 1)
+        right += 1
+
+    return length
+
+
+
+""" 
+Time Complexity -> O(n)
+Space Complexity -> O(n)
+"""
+
+
+
+
+
+
+
+
+
+
+
