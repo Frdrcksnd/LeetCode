882c75106414e189bbc64d8bde325df9aeceabe1
Frederick Osunde <frederick.osunde@workday.com>
2024-02-04 13:35:46 -0500

    solution for two sum

---
+++ snap_accelerator/week_1/two_sum.py
@@ -0,0 +1,9 @@
+
+def two_sum(self, nums: List[int], target: int) -> List[int]:
+    lookup = {}
+
+    for idx, num in enumerate(nums):
+        val = target - num
+        if val in lookup and lookup[val] != idx:
+            return [idx, lookup[val]]
+        lookup[num] = idx
